36019073911ef5d2d8c0bb535170e67788fc4f8f
Richard Cho <96richardohc@gmail.com>
2020-06-03 11:15:51 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -50,9 +50,14 @@ class Owner
   end
   
   def sell_pets
-    self.cats.each {|cat| cat.mood = "nervous"}
+    self.cats.each {|cat| 
+    cat.mood = "nervous"
+      cat.owner = nil
+    }
     self.dogs.each {|dog| dog.mood = "nervous"}
     self.dogs.each {|dog| dog.owner = nil}
-    self.cats.each {|cat| cat.owner = nil}
+    # self.cats.each {|cat| }
   end
+  
+  
 end
